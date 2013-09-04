9dfa7886564fcbf8db48f3d2e79f4c9537a2f040
rico becker <rico.becker@kf-interactive.com>
2013-09-04 12:45:32 +0200

    Initial Commit

---
+++ KFCocoaPodsPlugin/KFCocoaPodsPlugin.h
@@ -0,0 +1,13 @@
+//
+//  KFCocoaPodsPlugin.h
+//  KFCocoaPodsPlugin
+//
+//  Created by rick on 04.09.13.
+//  Copyright (c) 2013 KF Interactive. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface KFCocoaPodsPlugin : NSObject
+
+@end
---
+++ KFCocoaPodsPlugin/KFCocoaPodsPlugin.m
@@ -0,0 +1,54 @@
+//
+//  KFCocoaPodsPlugin.m
+//  KFCocoaPodsPlugin
+//
+//  Created by rick on 04.09.13.
+//    Copyright (c) 2013 KF Interactive. All rights reserved.
+//
+
+#import "KFCocoaPodsPlugin.h"
+
+@implementation KFCocoaPodsPlugin
+
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedPlugin = [[self alloc] init];
+    });
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+            [actionMenuItem release];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+@end
