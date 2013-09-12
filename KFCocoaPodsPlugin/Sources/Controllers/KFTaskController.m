eec6db799f4d290bb26154e9906c0c13a35c79e6
rico becker <rico.becker@kf-interactive.com>
2013-09-12 18:41:55 +0200

    changes to DSUnixTask for subtask execution

--- KFCocoaPodsPlugin/Sources/Controllers/KFTaskController.m
+++ KFCocoaPodsPlugin/Sources/Controllers/KFTaskController.m
@@ -24,66 +24,28 @@
 //
 
 #import "KFTaskController.h"
+#import <DSUnixTask/DSUnixTaskSubProcessManager.h>
 
 @implementation KFTaskController
 
 
-- (void)runShellCommand:(NSString *)command withArguments:(NSArray *)arguments directory:(NSString *)directory progress:(KFTaskControllerProgressBlock)progressBlock completion:(KFTaskControllerCompletionBlock)completionBlock
-{
-    NSTask *task = [NSTask new];
+- (void)runPodCommand:(NSArray *)arguments directory:(NSString *)directory outputHandler:(KFTaskOutputHandler)outpuBlock terminationHandler:(KFTaskTerminationHandler)terminationBlock failureHandler:(KFTaskFailureHandler)failureBlock
+{    
+    DSUnixTask *task = [DSUnixTaskSubProcessManager shellTask];
+    [task setCommand:@"pod"];
+    [task setWorkingDirectory:directory];
+    [task setArguments:arguments];
     
-    if (directory != nil)
+    [task setStandardErrorHandler:^(DSUnixTask *task, NSString *error)
     {
-        task.currentDirectoryPath = directory;
-    }
-    task.launchPath = command;
-    task.arguments  = arguments;
-    task.standardOutput = [NSPipe pipe];
-    task.standardError  = [NSPipe pipe];
-    
-    __block NSMutableData *outputData = [NSMutableData new];
-    __block NSMutableData *errorData = [NSMutableData new];
-
-    [[task.standardOutput fileHandleForReading] setReadabilityHandler:^(NSFileHandle *file)
-    {
-        NSData *availableData = [file availableData];
-        [outputData appendData:availableData];
-        if (progressBlock != nil)
-        {
-            progressBlock(task, [[NSString alloc] initWithData:availableData encoding:NSUTF8StringEncoding], nil);
-        }
+        NSLog(@"DSUnixTask error: %@", error);
     }];
     
-    [[task.standardError fileHandleForReading] setReadabilityHandler:^(NSFileHandle *file)
-    {
-        NSData *availableData = [file availableData];
-        [errorData appendData:availableData];
-        if (progressBlock != nil)
-        {
-            progressBlock(task, nil, [[NSString alloc] initWithData:availableData encoding:NSUTF8StringEncoding]);
-        }
-    }];
+    [task setStandardOutputHandler:outpuBlock];
+    [task setTerminationHandler:terminationBlock];
+    [task setFailureHandler:failureBlock];
     
-    [task setTerminationHandler:^(NSTask *task)
-    {
-        [task.standardOutput fileHandleForReading].readabilityHandler = nil;
-        [task.standardError fileHandleForReading].readabilityHandler  = nil;
-    }];
-    
-    @try
-    {
-        [task launch];
-        [task waitUntilExit];
-    }
-    @catch (NSException *exception)
-    {
-        completionBlock(task, NO, [[NSString alloc] initWithData:errorData encoding:NSUTF8StringEncoding], exception);
-        return;
-    }
-    @finally
-    {
-        completionBlock(task, YES, [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding], nil);
-    }
+    [task launch];
 }
 
 
